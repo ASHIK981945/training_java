8378d138629d3f378f035ddadd18f576468cc828
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-03-12 19:37:24 +0545

    java

--- FoodList.java
+++ FoodList.java
@@ -36,40 +36,6 @@ class Food {
     }
 }
 
-class Menu {
-    String category;
-    ArrayList<Food> menuItems;
-
-    Menu() {
-        menuItems = new ArrayList<>();
-    }
-
-    public void setcategory(String category) {
-        this.category = category;
-    }
-
-    public void setMenuItems(ArrayList<Food> foods) {
-        this.menuItems = foods;
-
-    }
-
-    public String getcategory() {
-        return category;
-
-    }
-
-    public ArrayList<Food> getMenuItems() {
-        return menuItems;
-    }
-
-    public void addItems(Food food) {
-        menuItems.add(food);
-    }
-    // public void removeItem(string ){
-
-    // }
-}
-
 public class FoodList {
     public static void main(String[] args) {
 
@@ -82,9 +48,9 @@ public class FoodList {
         // f2.setName("samoso");
         // f2.setPrice(102.3);
 
-        // // object
-        // // cannot put int ,
-        // // array is fix size
+        // object
+        // cannot put int ,
+        // array is fix size
         // ArrayList<Food> menu = new ArrayList<Food>();
         // menu.add(f1);
         // menu.add(f2);
@@ -93,26 +59,20 @@ public class FoodList {
         ArrayList<Food> list = new ArrayList<Food>();
         // list --> object .add(new Food --> class name ("chowin", 2.2));
 
-        // list.add(new Food("momo", 12.2));
-        // list.add(new Food("chowin1", 2.2));
-        // list.add(new Food("chowin2", 22.2));
-        // list.add(new Food("chowin3", 27.2));
-        // list.add(new Food("chowin4", 452.2));
-        // list.add(new Food("chowin12", 234.2));
+        list.add(new Food("momo", 12.2));
+        list.add(new Food("chowin1", 2.2));
+        list.add(new Food("chowin2", 22.2));
+        list.add(new Food("chowin3", 27.2));
+        list.add(new Food("chowin4", 452.2));
+        list.add(new Food("chowin12", 234.2));
 
-        // // camnot use tostring output come in hashcode
-        // // System.out.println(list);
+        // camnot use tostring output come in hashcode
+        // System.out.println(list);
 
-        // // for each
-        // for (Food food : list) {
-        // System.out.println(food);
-        // }
+        // for each
+        for (Food food : list) {
+            System.out.println(food);
+        }
 
-        Menu menu1 = new Menu();
-        menu1.setcategory("momo");
-        menu1.addItems(new Food("chickmomo", 230));
-        menu1.addItems(new Food("foodmomo", 670));
-        menu1.addItems(new Food("watermomo", 130));
-        System.out.println(menu1.getMenuItems());
     }
 }
